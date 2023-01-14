6ceb160e05b755950621b39ed1b45a757e25eb65
Joseph Z. Baturiano <josephbaturiano3@gmail.com>
2023-01-15 00:31:45 +0630

    Defining a search function

--- Binary.py
+++ Binary.py
@@ -4,18 +4,33 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
+    def add_child(self, data):
+        if data == self.data:
+            return  # node already exist
 
-def add_child(self, data):
-    if data == self.data:
-        return  # node already exist
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
 
-    if data < self.data:
-        if self.left:
-            self.left.add_child(data)
-        else:
-            self.left = BinarySearchTreeNode(data)
-    else:
-        if self.right:
-            self.right.add_child(data)
-        else:
-            self.right = BinarySearchTreeNode(data)
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
