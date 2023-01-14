0514d03bfefacac1f6f7d5c52135705dc38d6077
Joseph Z. Baturiano <josephbaturiano3@gmail.com>
2023-01-15 00:46:41 +0630

    Defining the build tree function to buil the binary tree

--- Binary.py
+++ Binary.py
@@ -102,3 +102,12 @@ class BinarySearchTreeNode:
             self.right = self.right.delete(min_val)
 
         return self
+
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+
+    return root
